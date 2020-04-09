90ab3bacfed34f4465a78087213d69dfbe22fba7
fushuai <fushuai@qunhemail.com>
2020-04-09 23:43:00 +0800

    raw: draw a triangle

--- raw/raw.js
+++ raw/raw.js
@@ -1,28 +1,37 @@
 function main() {
-    var gl = getWebGLContext();
+    var gl = getWebGLContext('canvas-container');
+
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT); // ?
 
     var program = createProgram(gl);
     gl.program = program;
     gl.useProgram(program);
 
-    gl.drawArrays(gl.TRIANGLE, 0, 3);
+    uploadData(gl);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
 main();
 
 function getWebGLContext(id) {
-    var dom = document.getElementById(id);
-    var context = dom.getWebGLContext();
+    var canvas = document.getElementById(id);
+    var context = canvas.getContext('webgl');
     return context;
 }
 
 function uploadData(gl) {
     var data = new Float32Array([
-        0, 0, 400, 0, 0, 300
+        0.0, 0.0, 0.4, 0.0, 0.0, 0.3
     ]);
 
     var buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ArrayElement, buffer);
-    gl.bufferData(gl.ArrayElement, vertices, gl.static);
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
 
     return buffer;
 }
@@ -30,21 +39,21 @@ function uploadData(gl) {
 
 
 function createProgram(gl) {
-    var vertexShader = createShader(gl, '', gl.vertexShader);
-    var fragmentShader = createShader(gl, '', gl.fragmentShader);
+    var vertexShader = createShader(gl, 'attribute vec4 a_Position; void main() {gl_Position = a_Position; gl_PointSize = 5.0;}', gl.VERTEX_SHADER);
+    var fragmentShader = createShader(gl, 'void main() {gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);}', gl.FRAGMENT_SHADER);
 
     var program = gl.createProgram();
-    program.attachShader(program, vertexShader);
-    program.attachShader(program, fragmentShader);
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
 
-    program.linkShaders();
+    gl.linkProgram(program);
 
     return program;
 }
 
 function createShader(gl, source, type) {
     var shader = gl.createShader(type);
-    shader.shaderSource(source);
+    gl.shaderSource(shader, source);
     gl.compileShader(shader);
     return shader;
 }
